3adc797c41758f994196899d56439f163ae37e83
Lerrrtaste <lerrrtaste@protonmail.com>
2022-07-13 15:36:51 +0200

    add dwm-swallow-6.2.diff

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
