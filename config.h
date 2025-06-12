808a74aa46f23e8f05613fc96785ff52b9ae0326
Laurenz Foglia <lerrrtaste@protonmail.com>
2025-06-13 01:57:26 +0200

    Add dwm-systray-20230922-9f88553.diff

--- config.h
+++ config.h
@@ -11,6 +11,15 @@ static const char *fonts[]          = {
 	"monospace:size=10",
 	/* "FiraCode Nerd Font:size=10::antialias=true:autohint=true", */
 };
+// systray
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
+
+
+
 /* static const char dmenufont[]       = "FiraCode Nerd Font:size=10:antialias=true:autohint=true"; */
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
